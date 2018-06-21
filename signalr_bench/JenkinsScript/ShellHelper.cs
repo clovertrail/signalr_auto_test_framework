0876a2e3f3e3c5a0f12caf7219047294842832a2
wanl <wanl@microsoft.com>
2018-06-21 18:00:15 +0800

    jenkins scripts

--- signalr_bench/JenkinsScript/ShellHelper.cs
+++ signalr_bench/JenkinsScript/ShellHelper.cs
@@ -148,7 +148,7 @@ namespace JenkinsScript
             var bench_type_list = serviceType;
             var bench_codec_list = hubProtocol;
             var bench_name_list = scenario;
-            cmd = $"cd /home/{agentConfig.User}/signalr_auto_test_framework/signalr_bench/Rpc/Bench.Client/; export bench_type_list='service'; export bench_codec_list='{hubProtocol}'; export bench_name_list='{scenario}'; export ConfigBlobContainerName='{argsOption.ContainerName}'; export AgentConfigFileName='{argsOption.AgentBlobName}';  export JobConfigFileName='{argsOption.JobBlobName}'; dotnet run -- -v {serviceType} -t {transportType} -p {hubProtocol} -s {scenario} -a '{argsOption.AgentConfigFile}' -j '{argsOption.JobConfigFile}' -o '/home/{agentConfig.User}/signalr-bench/{Environment.GetEnvironmentVariable("result_root")}/{bench_type_list}_{bench_codec_list}_{bench_name_list}/counters.txt'";
+            cmd = $"cd /home/{agentConfig.User}/signalr_auto_test_framework/signalr_bench/Rpc/Bench.Client/; export bench_type_list='{serviceType}'; export bench_codec_list='{hubProtocol}'; export bench_name_list='{scenario}'; export ConfigBlobContainerName='{argsOption.ContainerName}'; export AgentConfigFileName='{argsOption.AgentBlobName}';  export JobConfigFileName='{argsOption.JobBlobName}'; dotnet run -- -v {serviceType} -t {transportType} -p {hubProtocol} -s {scenario} -a '{argsOption.AgentConfigFile}' -j '{argsOption.JobConfigFile}' -o '/home/{agentConfig.User}/signalr-bench/{Environment.GetEnvironmentVariable("result_root")}/{bench_type_list}_{bench_codec_list}_{bench_name_list}/counters.txt'";
             Util.Log($"CMD: {agentConfig.User}@{agentConfig.Master}: {cmd}");
             var maxRetry = 100;
             for (var i = 0; i < maxRetry; i++)
@@ -175,13 +175,13 @@ namespace JenkinsScript
 
         }
 
-        public static (int, string) GenerateAllReports(List<string> hosts, AgentConfig agentConfig)
+        public static (int, string) GenerateAllReports(List<string> hosts, AgentConfig agentConfig, string serviceType, string transportType, string hubProtocol, string scenario)
         {
             var errCode = 0;
             var result = "";
             var cmd = "";
 
-            cmd = $"cd /home/{agentConfig.User}/signalr-bench/; sh gen_all_report.sh; sh publish_report.sh; sh gen_summary.sh;";
+            cmd = $"cd /home/{agentConfig.User}/signalr-bench/; export bench_type_list='{serviceType}'; export bench_codec_list='{hubProtocol}'; export bench_name_list='{scenario}'; sh gen_all_report.sh; sh publish_report.sh; sh gen_summary.sh;";
             Util.Log($"CMD: {agentConfig.User}@{agentConfig.Master}: {cmd}");
             (errCode, result) = ShellHelper.RemoteBash(agentConfig.User, agentConfig.Master, agentConfig.SshPort, agentConfig.Password, cmd);
 
@@ -197,13 +197,13 @@ namespace JenkinsScript
             return (errCode, result);
 
         }
-        public static (int, string) GenerateSingleReport(List<string> hosts, AgentConfig agentConfig)
+        public static (int, string) GenerateSingleReport(List<string> hosts, AgentConfig agentConfig, string serviceType, string transportType, string hubProtocol, string scenario)
         {
             var errCode = 0;
             var result = "";
             var cmd = "";
 
-            cmd = $"cd /home/{agentConfig.User}/signalr-bench/; sh gen_html.sh;";
+            cmd = $"cd /home/{agentConfig.User}/signalr-bench/; export bench_type_list='{serviceType}'; export bench_codec_list='{hubProtocol}'; export bench_name_list='{scenario}'; sh gen_html.sh;";
             Util.Log($"CMD: {agentConfig.User}@{agentConfig.Master}: {cmd}");
             (errCode, result) = ShellHelper.RemoteBash(agentConfig.User, agentConfig.Master, agentConfig.SshPort, agentConfig.Password, cmd);
 
