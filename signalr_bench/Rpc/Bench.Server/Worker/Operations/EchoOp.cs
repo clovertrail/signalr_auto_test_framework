d0ac78eb9006e92fc6f4b23b65dff89b928322d4
wanl <wanl@microsoft.com>
2018-06-29 03:20:07 +0800

    debug start client job7

--- signalr_bench/Rpc/Bench.Server/Worker/Operations/EchoOp.cs
+++ signalr_bench/Rpc/Bench.Server/Worker/Operations/EchoOp.cs
@@ -35,7 +35,7 @@ namespace Bench.RpcSlave.Worker.Operations
             StartSendMsg();
 
             // wait to stop
-            Task.Delay(TimeSpan.FromSeconds(_tk.JobConfig.Duration + _tk.JobConfig.Interval*5)).ContinueWith(t => {
+            Task.Delay(TimeSpan.FromSeconds(_tk.JobConfig.Duration + 30)).ContinueWith(t => {
                 SaveCounters();
             }).Wait();
 
